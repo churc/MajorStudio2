36197485d77a2469c20c9ef2f8309fed73e11c17
Clare <ccinflux@hotmail.com>
2019-05-10 22:30:18 -0400

    Update indexNatArtMus2015-18hiring3.js

--- js/indexNatArtMus2015-18hiring3.js
+++ js/indexNatArtMus2015-18hiring3.js
@@ -102,9 +102,10 @@ var n = 10;
 			// 				var color = "blue";
 			// 				return color;								
 			// 			}
-			// 
-////color every 2 bars differently
-////color by color scheme
+			//
+			
+////draw percentage lines, color every 2 bars differently
+
 			bars1.append("line")
 					.attr("fill", "none")
 					// .attr("stroke", "none")
@@ -115,7 +116,7 @@ var n = 10;
 						// bars1.attr("stroke", "sienna")
 						} else {
 							// bars1.attr("stroke", "gold")
-								var color = "coral"
+								var color = "GOLDENROD"
 								return color
 						}
 						})
@@ -191,6 +192,51 @@ var n = 10;
 								return i * 1;
 					}
 					})
+
+////vertical line to show national percentage
+
+let natpercentage = bars1.append("line")
+// let natpercentage = g.append("line")
+							.attr("fill", "none")
+							.attr("stroke", "#f5f4f9")
+							.attr("opacity", 0.9)
+							// .style("stroke-dasharray", 3,3)
+							.attr("stroke-width", 0.2)
+							.attr('x1', function(d,i) {
+												return xScale(68);
+									})
+							.attr('x2', function(d,i) {
+												return xScale(68);
+									})
+							.attr('y1', height/2)
+							.attr('y2', height*1.7)
+							
+////line to label vertical percent
+
+let natpercentlabel =	g.append("path")
+						.data(data)
+							.attr("fill", "none")
+							.attr("stroke", "#f5f4f9")
+							.attr("opacity", 0.9)
+							.attr("stroke-width", 0.3)
+							
+							.attr("d", "M "+xScale(68)+ ",100, L 520,50 L 670,50 ")  ////////can I change these to scales? or do I use margin.left etc?
+							
+							// .attr('M', function(d,i) {
+							// 					return xScale(68);
+							// 		})
+							// .attr('x2', function(d,i) {
+							// 					return xScale(68);
+							// 		})
+							// .attr('y1', height/8)
+							// .attr('y2', height*1.7)
+					
+				g.append("text")   ////text on line label
+						.text('67% white, total U.S. population, 2010 U.S. Census')
+						.style("fill", "#f5f4f9")
+						.attr('font-size', '0.8em')
+						.attr('x', 525)
+						.attr('y', 45)
 					
 						// .on('mouseenter', (d,i,j) => {
 						// 	console.log("hover")
@@ -278,10 +324,10 @@ var n = 10;
 						})
 					  .attr("fill", function(d,i) {
 					  		if(i % 2 == 0){
-							var color = "palevioletred";
+							var color = "CHOCOLATE";
 							return color; 
 						} else {
-							var color = "palevioletred";
+							var color = "CHOCOLATE";
 							return color; 							
 						}
 						 // 	if(i <= 1){
@@ -559,7 +605,7 @@ let xlabel = g.append('text')
             .text("percent")
               
 
-			let headingpercent = g.append('text')
+let headingpercent = g.append('text')
 					.text('New Hiring Since 2014: Percent Comparison Among Repeat 2015 & 2018 Survey Participants')
 					.attr('x', -margin.left/20)
 					.attr('y', -margin.top/1.2)
@@ -590,467 +636,3 @@ let xlabel = g.append('text')
 					.attr('font-size', '0.95em');
 					
 };
-
-/////==================
-
-///////2018 parallel lines 
-////National Art Museum Staff Survey Intellectual Leadership Positions 
-////by number
-// var n = 6;
-
-// 				// for horizontal
-				
-// 				let xScale3 = d3.scaleLinear()
-// 					.domain([0, 305+30])
-// 					.range([0, width]);
-
-// 				let yScale3 = d3.scaleLinear()
-// 					.domain([0, 305]) 
-// 					.range([height, 0]);
-					
-// 					/////bars ascending
-// 				// let ySc = d3.scaleLinear()
-// 				// 	.domain([0, 220]) ////using just the numbers here
-// 				// 	.range([height, 0]);
-// // var line = d3.line()
-
-// 		let bars3 = g2.selectAll("rect")
-// 					.data(data)
-// 					.enter()
-// 					.append("g")
-// 					.attr('class', 'barthin3')
-			
-// 				console.log(data);
-
-// 			bars3.append("rect")
-// 					.attr("fill", "none")
-// 					.attr("stroke", "sienna")
-// 					.attr('x', function(d,i) { 
-// 						return 0; 
-// 					})
-// 					.attr('y', (d, i) => { //// d is shorthand for element in the data and i is index
-// 						return i *33;
-// 						// return line(d.year15) 
-// 					})
-// 					.attr('width', function(d,i) {
-// 						console.log(d.year18)
-// 						return xScale(d.year18); 
-// 					})
-// 					.attr('height', 1) ///bar width
-// 						// .on('mouseenter', (d,i,j) => {
-// 						// 	console.log("hover")
-// 					 // 	title2.text(d[i].year15)
-// 					 // 	d3.select(j[i])
-// 					 // 	.style('fill', 'blue')
-//       //         .style('opacity', '.5');
-// 						// })
-// 	     //   .on('mouseout', (d, i,j) => {
-// 		    //         // console.log(d);
-// 		    //         title2.text((d) => { return ""; });
-// 		    //   	 d3.select(j[i])
-// 					 // 	.style('fill', 'sienna')
-// 					 // 	 .style('opacity', '1');
-// 						// })
-	        	
-// 	    bars3.append("circle")
-//   			.data(data)
-// 			  .attr("r", 8)
-// 			  .attr("fill", "pink")
-// 			  // position the circles right where the path elements end
-// 			  .attr('cy', (d, i) => { //// d is shorthand for element in the data and i is index
-// 						return i * 33;
-// 					})
-// 			  .attr('cx', (d,i) => { 
-// 			  	return  xScale(d.year18); 
-// 			  })
-	
-
-			// /////add transparent rect on top so can select bars more easily
-				// bars1.append("rect")
-				// 	.enter()
-				// 	.attr('class', 'barthin')
-				// 	.attr('x', margin.right)
-				// 	.attr('width', width)
-				// 	.attr('height', height*8)
-				// 	.attr('fill', 'transparent')
-				// 			.on('mouseenter', (d,i,j) => {
-				// 			console.log("hover")
-				// 	  	title2.text(d.year15)
-				// 	  	d3.select(j[i])
-				// 	  	.style('fill', 'pink')
-    //           .style('opacity', '.5');
-				// 		})
-	   //     .on('mouseout', (d, i,j) => {
-		  //           // console.log(d);
-		  //           title2.text((d) => { return "NYC Museum Staff"; });
-		  //     	 d3.select(j[i])
-				// 	  	.style('fill', 'blue')
-				// 	  	 .style('opacity', '1');
-				// 		})
-
-
-// 				////number each bar 
-// var numlabel = bars3.append('text')
-// 					.text((d,i) => {
-// 						return d.year18;
-// 					})
-// 					.attr('y', (d, i) => { //// d is shorthand for element in the data and i is index
-// 						return i * 33;
-// 					})
-// 					.attr('x', (d, i) => { return  xScale(d.year18); })
-// 					// .attr('transform', (d) => { return 'translate(' + 20 + ', ' + (500 - yScale(d) ); });
-// 					.style("font-size", "60%")
-// 					.style('fill', 'white') ///////numbers on top of bars
-// 					.attr("text-anchor", "middle")
-// 				.attr("transform", "translate(30, 4)")
-	
-
-// 				//////label text for each bar
-// 	bars3.append('text')
-// 					.data(data)
-// 					.attr("class", "numlabel")
-// 					.attr("text-anchor", "bottom")
-// 					.text((d, i) => {
-// 						return d.raceethnicity;
-// 					})
-// 					.attr("x", -margin.left/2)
-// 					.attr('y', (d, i) => { //// d is shorthand for element in the data and i is index
-// 						return i * 33;
-// 					})
-// 					// .attr("x", margin.left/10)
-// 					.attr("transform", "translate(-20, -12)")
-// 					.style("font-family", "sans-serif")
-// 					.style("font-size", "65%")
-// 					.style('fill', 'dark grey') 
-				
-
-
-// // 				////  numbers on the axis 
-// 				let yAxis3 = g2.append('g') // just append axis to global space
-// 					.style('fill', 'none')
-// 					.attr('stroke', 'grey')
-// 					.attr('path', 'red')
-// 					.attr('stroke-width', 0.3)
-// 					.attr("transform", `translate(0, ${-margin.top} )`)
-// 					.call(d3.axisLeft(xScale) //call axisLeft to use yScale for axis ticks and scale
-// 						.tickSize(0)
-// 						.ticks(0)
-// 						.tickFormat("") ////empty no text
-// 					)
-// 					.selectAll("text")
-// 					.attr("transform", "rotate(-65)")
-// 					.attr("text-anchor", "end");
-
-// 				// let xAxis = g.append("g")
-// 				// 	.attr("class", "y axis")
-// 				// 	.attr('fill', 'none')
-// 				// 	.attr('stroke', 'grey')
-// 				// 	.attr('stroke-width', 0.3)
-// 				// 	.attr("transform", `translate(0, ${height+margin.bottom/0.8 } )`)
-// 				// 	.call(d3.axisBottom(xScale)
-// 				// 		////above move the axis to place that the chart ends - within the margins
-// 				// 		.tickSize(3)
-// 				// 		.ticks(5)
-// 				// 	);
-	
-
-// 			let heading3 = g2.append('text')
-// 					// .text('National Art Museum Staff Survey Intellectual Leadership Positions, Total Hires')
-// 					.text('National Art Museum Staff Survey Intellectual Leadership Positions')
-// 					.attr('x', -margin.left)
-// 					// .attr('y', height+margin.top+margin.top/0.5)
-// 					.attr('y', height+margin.top+margin.top/0.35)
-// 					.attr('font-size', '100%');
-
-// 			let title3 = g2.append('text')
-// 					.text('2018')
-// 					.attr('x', -margin.left)
-// 					.attr('y', height+margin.top+margin.top/0.5)
-// 					.attr('font-size', '100%');
-					
-// 			let heading23 = g2.append('text')
-// 					.text('excluding white,')
-// 					.attr('x', -margin.left)
-// 					.attr('y', height+margin.top/0.7)
-// 					.attr('font-size', '70%');
-
-// 			let title23 = g2.append('text')
-// 					.text('non-Hispanic')
-// 					.attr('x', -margin.left)
-// 					.attr('y', height+margin.top/0.5)
-// 					.attr('font-size', '70%');
-					
-// ///////=======
-
-// ///////2015 Hispanic / non-Hispanic parallel lines
-// var n = 2;
-
-// 				// for horizontal
-				
-// 				let xScale1 = d3.scaleLinear()
-// 					.domain([0, 305+30])
-// 					.range([0, width]);
-
-// 				let yScale1 = d3.scaleLinear()
-// 					.domain([0, 305]) 
-// 					.range([height, 0]);
-					
-// 					/////bars ascending
-// 				// let ySc = d3.scaleLinear()
-// 				// 	.domain([0, 220]) ////using just the numbers here
-// 				// 	.range([height, 0]);
-// // var line = d3.line()
-
-// 		let bars2 = g1.selectAll("rect")
-// 					// .data(data)
-// 					.enter()
-// 					.append("g")
-// 					.attr('class', 'barthin3')
-			
-// 				console.log(data);
-
-// 			bars2.append("rect")
-// 					.attr("fill", "none")
-// 					.attr("stroke", "sienna")
-// 					.attr('x', function(d,i) { 
-// 						return 0; 
-// 					})
-// 					.attr('y', (d, i) => { //// d is shorthand for element in the data and i is index
-// 						return i *33;
-// 						// return line(d.year15) 
-// 					})
-// 					.attr('width', function(d,i) {
-// 						console.log(d.year18)
-// 						return xScale(d.year18); 
-// 					})
-// 					.attr('height', 1) ///bar width
-// 						// .on('mouseenter', (d,i,j) => {
-// 						// 	console.log("hover")
-// 					 // 	title2.text(d[i].year15)
-// 					 // 	d3.select(j[i])
-// 					 // 	.style('fill', 'blue')
-//       //         .style('opacity', '.5');
-// 						// })
-// 	     //   .on('mouseout', (d, i,j) => {
-// 		    //         // console.log(d);
-// 		    //         title2.text((d) => { return ""; });
-// 		    //   	 d3.select(j[i])
-// 					 // 	.style('fill', 'sienna')
-// 					 // 	 .style('opacity', '1');
-// 						// })
-	        	
-// 	    bars2.append("circle")
-//   			.data(data)
-// 			  .attr("r", 8)
-// 			  .attr("fill", "pink")
-// 			  // position the circles right where the path elements end
-// 			  .attr('cy', (d, i) => { //// d is shorthand for element in the data and i is index
-// 						return i * 33;
-// 					})
-// 			  .attr('cx', (d,i) => { 
-// 			  	return  xScale(d.year18); 
-// 			  })
-	
-
-// 			// /////add transparent rect on top so can select bars more easily
-// 				// bars1.append("rect")
-// 				// 	.enter()
-// 				// 	.attr('class', 'barthin')
-// 				// 	.attr('x', margin.right)
-// 				// 	.attr('width', width)
-// 				// 	.attr('height', height*8)
-// 				// 	.attr('fill', 'transparent')
-// 				// 			.on('mouseenter', (d,i,j) => {
-// 				// 			console.log("hover")
-// 				// 	  	title2.text(d.year15)
-// 				// 	  	d3.select(j[i])
-// 				// 	  	.style('fill', 'pink')
-//     //           .style('opacity', '.5');
-// 				// 		})
-// 	   //     .on('mouseout', (d, i,j) => {
-// 		  //           // console.log(d);
-// 		  //           title2.text((d) => { return "NYC Museum Staff"; });
-// 		  //     	 d3.select(j[i])
-// 				// 	  	.style('fill', 'blue')
-// 				// 	  	 .style('opacity', '1');
-// 				// 		})
-
-
-// 				////number each bar 
-// var numlabel =	bars2.append('text')
-// 					.text((d,i) => {
-// 						return d.year18;
-// 					})
-// 					.attr('y', (d, i) => { //// d is shorthand for element in the data and i is index
-// 						return i * 33;
-// 					})
-// 					.attr('x', (d, i) => { return  xScale(d.year18); })
-// 					// .attr('transform', (d) => { return 'translate(' + 20 + ', ' + (500 - yScale(d) ); });
-// 					.style("font-size", "60%")
-// 					.style('fill', 'white') ///////numbers on top of bars
-// 					.attr("text-anchor", "middle")
-// 				.attr("transform", "translate(30, 4)")
-	
-
-// 				//////label text for each bar
-// 	bars2.append('text')
-// 					.data(data)
-// 					.attr("class", "numlabel")
-// 					.attr("text-anchor", "bottom")
-// 					.text((d, i) => {
-// 						return d.raceethnicity;
-// 					})
-// 					.attr("x", -margin.left/2)
-// 					.attr('y', (d, i) => { //// d is shorthand for element in the data and i is index
-// 						return i * 33;
-// 					})
-// 					// .attr("x", margin.left/10)
-// 					.attr("transform", "translate(-20, -12)")
-// 					.style("font-family", "sans-serif")
-// 					.style("font-size", "65%")
-// 					.style('fill', 'dark grey') 
-				
-
-
-// // 				////  numbers on the axis 
-// 				let yAxis2 = g1.append('g') // just append axis to global space
-// 					.style('fill', 'none')
-// 					.attr('stroke', 'grey')
-// 					.attr('path', 'red')
-// 					.attr('stroke-width', 0.3)
-// 					.attr("transform", `translate(0, ${-margin.top} )`)
-// 					.call(d3.axisLeft(xScale) //call axisLeft to use yScale for axis ticks and scale
-// 						.tickSize(0)
-// 						.ticks(0)
-// 						.tickFormat("") ////empty no text
-// 					)
-// 					.selectAll("text")
-// 					.attr("transform", "rotate(-65)")
-// 					.attr("text-anchor", "end");
-
-// 				// let xAxis = g.append("g")
-// 				// 	.attr("class", "y axis")
-// 				// 	.attr('fill', 'none')
-// 				// 	.attr('stroke', 'grey')
-// 				// 	.attr('stroke-width', 0.3)
-// 				// 	.attr("transform", `translate(0, ${height+margin.bottom/0.8 } )`)
-// 				// 	.call(d3.axisBottom(xScale)
-// 				// 		////above move the axis to place that the chart ends - within the margins
-// 				// 		.tickSize(3)
-// 				// 		.ticks(5)
-// 				// 	);
-	
-
-// 			let heading12 = g1.append('text')
-// 					.text('National Art Museum Staff Survey')
-// 					.attr('x', -margin.left/2)
-// 					.attr('y', height+margin.top+margin.top/0.5)
-// 					.attr('font-size', '100%');
-
-// 			let title12 = g1.append('text')
-// 					.text('2018')
-// 					.attr('x', -margin.left)
-// 					.attr('y', height+margin.top+margin.top/0.5)
-// 					.attr('font-size', '100%');
-					
-// 			let heading13 = g1.append('text')
-// 					.text('excluding white,')
-// 					.attr('x', -margin.left)
-// 					.attr('y', height+margin.top/0.7)
-// 					.attr('font-size', '70%');
-
-// 			let title13 = g1.append('text')
-// 					.text('non-Hispanic')
-// 					.attr('x', -margin.left)
-// 					.attr('y', height+margin.top/0.5)
-// 					.attr('font-size', '70%');
-					
-// ///////////======					
-
-
-
-
-  // bars1.append("circle")
-  // 			.data(data)
-  // 			// .circle((d,i) => {
-		// 			// 	return d.years15;
-		// 			// })
-		// 	  .attr("r", 8)
-		// 	  // position the circles right where the path elements end
-		// 	  // .attr("cy", (d, i) => yScale(i) + yScale() / 2)
-		// 	  .attr('cy', (d, i) => { //// d is shorthand for element in the data and i is index
-		// 				return i * 42;
-		// 			})
-		// 	  // transition them horizontally and jointly with the path elements
-		// 	  // .attr("cx", (d) => 0)
-		// 	  // .transition()
-		// 	  // .duration(1000)
-		// 	  // .delay((d, i) => 100 + 150 * i)
-		// 	  .attr('cx', (d,i) => { return  xScale(d.year15); })
-		// 	  // .attr("cx", (d,i) => xScale(d)); 
-		// // //Call the y axis in a group tag
-///////////\\\\\
-// 				var n = 6; // number of samples
-//     		m = 2; // number of series
-			
-
-// 				// let xScale0 = d3.scaleLinear()
-// 				// 	.domain([0, n])
-// 				// 	.range([0, width]);
-				
-// 				let yScale = d3.scaleLinear()
-// 					// .domain([0, d3.max(ethboardallg) + 18]) ////using just the numbers here
-// 						.domain([0,305])
-// 						.range([height, 0]);
-						
-// 				let xScale0 = d3.scaleBand()
-// 						  .domain(d3.range(n))
-// 	    			  .range([0, width], .2);
-
-// 				let xScale1 = d3.scaleBand()
-// 					// .domain([0, d3.max(ethboardallg)]) ////using just the numbers here
-// 					// .domain([0, d3.max(ethboardallg) + 18]) ///ADDS HEIGHT TO THE AXIS
-// 						.domain(d3.range(m))
-//     				.range([0, xScale0.bandwidth() - 10]);
-					
-		
-// // var y = d3.scaleLinear()
-// //     .domain([0, 1])
-// //     .range([height, 0]);
-
-// // var x0 = d3.scaleBand()
-// //     .domain(d3.range(n))
-// //     .range([0, width], .2);
-
-// // var x1 = d3.scaleBand()
-// //     .domain(d3.range(m))
-// //     .range([0, x0.bandwidth() - 10]);
-
-// console.log(data[0].year15)
-
-// 			let bars1 = g.selectAll("rect")
-// 					.data(data)
-// 					.enter()
-// 					.append("g")
-// 					// .attr("transform", "translate(0, -150)") ////so that bars are not cut off on base
-// 					.attr('x', (d, i) => { //// d is shorthand for element in the data and i is index
-// 						return i * 42;
-// 					})
-// 					.attr('width', 10) ///bar width
-// 					//////mouseover for text
-// 					// .on('mouseover', function(d) {
-// 					//   			        bars1.text(d.name)  ///////?????
-// 					//   			   	d3.select(this).select('text')
-// 					//       				.text((d) => { return 'number ' + d; });
-// 					//   			      });
-// 					.text((d) => { return 'number ' + d; });
-
-// 				// .on('mouseover', function() {
-// 				//             d3.select(this).selectAll('.bar').style('fill-opacity',0.5)
-// 				//             d3.select(this).selectAll('.bar-name').style('visibility','visible')
-// 				//         })
-// 				//  .on('mouseout', function() {
-// 				//             d3.select(this).selectAll('.bar').style('fill-opacity',1)
-// 				//             d3.select(this).selectAll('.bar-name').style('visibility','hidden')
-// 				//         })
-
