ab410fd1083a4a8a6e0af2c13d7605e838ae8577
Clare <ccinflux@hotmail.com>
2019-05-18 22:32:32 -0400

    Update indexSpace_Full_bubble_NumDateAccessionsPre15FINALpercent.js

--- js/indexSpace_Full_bubble_NumDateAccessionsPre15FINALpercent.js
+++ js/indexSpace_Full_bubble_NumDateAccessionsPre15FINALpercent.js
@@ -592,7 +592,7 @@ let heading15 = g.append('text')  //// append text to global
                 .style('font-size', '0.95em')
 
 let heading16 = g.append('text')  //// append text to global
-                .text('9,961 Artworks')
+                .text('9,961 artworks   ::   97.18%')
                 .attr('x', width/2.8)
                 .attr('y', height+margin.bottom/1.1)
                 .style("fill", "#f5fcf7")
